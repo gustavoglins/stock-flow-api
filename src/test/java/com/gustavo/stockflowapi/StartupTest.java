251e64df108920250f994e0db1fb4b8b15814a85
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-02 18:34:49 -0300

    Initial Commit

---
+++ src/test/java/com/gustavo/stockflowapi/StartupTest.java
@@ -0,0 +1,13 @@
+package com.gustavo.stockflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
