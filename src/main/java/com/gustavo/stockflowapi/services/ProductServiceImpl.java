4635fe4a0535ddfa2a3b60e3545e8bfcce4ccad7
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-04 13:26:16 -0300

    feat: add product data validation in ProductServiceImpl

--- src/main/java/com/gustavo/stockflowapi/services/ProductServiceImpl.java
+++ src/main/java/com/gustavo/stockflowapi/services/ProductServiceImpl.java
@@ -1,6 +1,9 @@
 package com.gustavo.stockflowapi.services;
 
 import com.gustavo.stockflowapi.dtos.ProductDTO;
+import com.gustavo.stockflowapi.exceptions.InvalidProductDataException;
+import com.gustavo.stockflowapi.exceptions.ProductNotFoundException;
+import com.gustavo.stockflowapi.exceptions.UnexpectedException;
 import com.gustavo.stockflowapi.model.Product;
 import com.gustavo.stockflowapi.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -17,31 +20,58 @@ public class ProductServiceImpl implements ProductService {
         this.repository = repository;
     }
 
+    public boolean productDataValidation(ProductDTO productDTO) {
+        if (productDTO.name().isEmpty() || productDTO.name().isBlank()) {
+            throw new InvalidProductDataException("Name cannot be empty or null");
+        }
+        if (productDTO.description().isEmpty() || productDTO.description().isBlank()) {
+            throw new InvalidProductDataException("Description cannot be empty or null");
+        }
+        if (productDTO.price().doubleValue() < 0) {
+            throw new InvalidProductDataException("Price must be greater than 0");
+        }
+        if (productDTO.quantity() == null) {
+            throw new InvalidProductDataException("Quantity cannot be null, must be 0 or greater");
+        }
+
+        return true;
+    }
+
     @Override
     public ProductDTO create(ProductDTO productDTO) {
-        return new ProductDTO(repository.save(new Product(productDTO)));
+        if (productDataValidation(productDTO)) {
+            return new ProductDTO(repository.save(new Product(productDTO)));
+        } else {
+            throw new UnexpectedException("Unexpected error! class ProductServiceImpl, method create");
+        }
     }
 
     @Override
     public ProductDTO update(ProductDTO productDTO) {
         Optional<Product> optionalProduct = repository.findById(productDTO.id());
-        if (optionalProduct.isPresent()) {
-            Product selectedProduct = optionalProduct.get();
 
-            selectedProduct.setName(productDTO.name());
-            selectedProduct.setDescription(productDTO.description());
-            selectedProduct.setPrice(productDTO.price());
-            selectedProduct.setQuantity(productDTO.quantity());
+        if(productDataValidation(productDTO)){
+            if (optionalProduct.isPresent()) {
+                Product selectedProduct = optionalProduct.get();
 
-            return new ProductDTO(repository.save(selectedProduct));
+                selectedProduct.setName(productDTO.name());
+                selectedProduct.setDescription(productDTO.description());
+                selectedProduct.setPrice(productDTO.price());
+                selectedProduct.setQuantity(productDTO.quantity());
+
+                return new ProductDTO(repository.save(selectedProduct));
+            } else {
+                throw new ProductNotFoundException("Product not found");
+            }
+
         } else {
-            throw new RuntimeException("Product not found"); //TODO create a specify exception for this case
+            throw new UnexpectedException("Unexpect error! class ProductServiceImpl, method update");
         }
     }
 
     @Override
     public ProductDTO findById(Long id) {
-        return new ProductDTO(repository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"))); //TODO create a specify exception for this case
+        return new ProductDTO(repository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product not found")));
     }
 
     @Override
@@ -56,10 +86,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void delete(Long id) {
         Optional<Product> optionalProduct = repository.findById(id);
-        if(optionalProduct.isPresent()){
+        if (optionalProduct.isPresent()) {
             repository.deleteById(id);
-        } else{
-            throw new RuntimeException("Product not found");//TODO create a specify exception for this case
+        } else {
+            throw new ProductNotFoundException("Product not found");
         }
     }
 }
