66faf5e2e59d47501ab9c05a74dd84e39583876a
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-06 12:18:30 -0300

    feat: documenting ProductController

---
+++ src/main/java/com/gustavo/stockflowapi/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.gustavo.stockflowapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Stock Flow API")
+                        .version("v1")
+                        .description("API for Stock Flow application")
+                        .termsOfService("https://github.com/gustavoglins")
+                        .license(new License().name("StockFlow 1.0").url("https://github.com/gustavoglins"))
+                );
+    }
+}
--- src/main/java/com/gustavo/stockflowapi/controllers/ProductController.java
+++ src/main/java/com/gustavo/stockflowapi/controllers/ProductController.java
@@ -2,6 +2,9 @@ package com.gustavo.stockflowapi.controllers;
 
 import com.gustavo.stockflowapi.dtos.ProductDTO;
 import com.gustavo.stockflowapi.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,7 +24,13 @@ public class ProductController {
         this.service = service;
     }
 
-    @PostMapping
+    @Operation(summary = "Creates a new Product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Product successfully created"),
+            @ApiResponse(responseCode = "400", description = "Invalid product data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<ProductDTO> create(@RequestBody @Valid ProductDTO productDTO) {
         ProductDTO createdProduct = service.create(productDTO);
         return ResponseEntity
@@ -29,7 +38,14 @@ public class ProductController {
                 .body(createdProduct);
     }
 
-    @PutMapping
+    @Operation(summary = "Updates a existing Product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Product successfully updated"),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid product data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PutMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<ProductDTO> update(@RequestBody @Valid ProductDTO productDTO) {
         ProductDTO updatedProduct = service.update(productDTO);
         return ResponseEntity
@@ -37,7 +53,13 @@ public class ProductController {
                 .body(updatedProduct);
     }
 
-    @GetMapping("/{id}")
+    @Operation(summary = "Finds a Product by ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Product found"),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error"),
+    })
+    @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO foundProduct = service.findById(id);
         return ResponseEntity
@@ -45,13 +67,24 @@ public class ProductController {
                 .body(foundProduct);
     }
 
-    @GetMapping
+    @Operation(summary = "List all Products")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Products successfully retrieved"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping(produces = "application/json")
     public List<ProductDTO> listAll() {
         return service.findAll();
     }
 
+    @Operation(summary = "Deletes a Product by ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Product successfully deleted"),
+            @ApiResponse(responseCode = "404", description = "Product not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
