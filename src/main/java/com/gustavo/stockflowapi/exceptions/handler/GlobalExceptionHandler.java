85425bcf8c223a4809b30f4413d392771dd95500
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-06 12:40:30 -0300

    fix: change in GlobalExceptionHandler (was missing @ExceptionHandler)

--- src/main/java/com/gustavo/stockflowapi/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/gustavo/stockflowapi/exceptions/handler/GlobalExceptionHandler.java
@@ -7,22 +7,27 @@ import com.gustavo.stockflowapi.exceptions.UnexpectedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(UnexpectedException.class)
     public ResponseEntity<String> handlerUnexpectException(UnexpectedException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 
+    @ExceptionHandler(InvalidProductDataException.class)
     public ResponseEntity<String> handleInvalidProductDataException(InvalidProductDataException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
+    @ExceptionHandler(DuplicateProductException.class)
     public ResponseEntity<String> handleDuplicateProductException(DuplicateProductException e){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
+    @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
