490aa5b157d6d6796581e92c792f51cc7d8ba9de
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-18 20:01:02 -0300

    feat: create UserRepository

---
+++ src/main/java/com/stockflow/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.stockflow.repositories;
+
+import com.stockflow.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
