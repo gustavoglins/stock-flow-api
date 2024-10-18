c2278df4b31923aaf79afe258ba5a1812bd7c7fd
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-18 14:52:08 -0300

    Initial commit

---
+++ src/main/java/com/stockflow/StockFlowApplication.java
@@ -0,0 +1,11 @@
+package com.stockflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFlowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockFlowApplication.class, args);
+    }
+}
---
+++ src/test/java/com/stockflow/StockFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
