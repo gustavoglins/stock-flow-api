737a2f6af40797b11e534380156fee0aacd64fb7
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-19 10:41:21 -0300

    refactor: upgrade in GlobalExceptionHandler

--- src/main/java/com/stockflow/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/stockflow/exceptions/handler/GlobalExceptionHandler.java
@@ -4,33 +4,51 @@ import com.stockflow.exceptions.UserNotFoundException;
 import com.stockflow.exceptions.reponse.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ResponseEntity<ExceptionResponse> buildResponse(Exception exception, HttpStatus status, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                exception.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(status).body(exceptionResponse);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleGenericExceptions(Exception exception, WebRequest request) {
+        return buildResponse(exception, HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleValidationsExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
         ExceptionResponse exceptionResponse = new ExceptionResponse(
-                exception.getMessage(),
-                request.getDescription(false),
+                "Validation errors occurred.",
+                errors.toString(),
                 LocalDateTime.now()
         );
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ExceptionResponse> handleUserNotFoundException(UserNotFoundException exception, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                exception.getMessage(),
-                request.getDescription(false),
-                LocalDateTime.now()
-        );
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
+    public ResponseEntity<ExceptionResponse> handleUserNotFoundExceptions(UserNotFoundException exception, WebRequest request) {
+        return buildResponse(exception, HttpStatus.NOT_FOUND, request);
     }
 }
