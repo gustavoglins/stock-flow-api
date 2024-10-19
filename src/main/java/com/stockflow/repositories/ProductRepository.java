47e1633ef737c07d8fb649d30e8903319dc5f73e
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-19 15:41:24 -0300

    feat: create Product repository

---
+++ src/main/java/com/stockflow/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.stockflow.repositories;
+
+import com.stockflow.model.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
