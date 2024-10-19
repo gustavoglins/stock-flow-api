29cb7cf30135f18259f464dbae5ea31f10412eff
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-19 11:05:29 -0300

    feat: adding log in UserServiceImpl

--- src/main/java/com/stockflow/services/UserServiceImpl.java
+++ src/main/java/com/stockflow/services/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.stockflow.dto.UserDTO;
 import com.stockflow.exceptions.UserNotFoundException;
 import com.stockflow.model.user.User;
 import com.stockflow.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.stream.Collectors;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     private final UserRepository repository;
 
     public UserServiceImpl(UserRepository repository) {
@@ -22,38 +25,53 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO create(UserDTO userDTO) {
-        return new UserDTO(repository.save(new User(userDTO)));
+        logger.info("Creating a new user with login: {}", userDTO.login());
+        User createdUser = repository.save(new User(userDTO));
+        logger.info("User created successfully with ID: {}", createdUser.getId());
+        return new UserDTO(createdUser);
     }
 
     @Override
     public UserDTO update(UserDTO userDTO) {
+        logger.info("Updating user with ID: {}.", userDTO.id());
         Optional<User> optionalUser = repository.findById(userDTO.id());
+
         if (optionalUser.isPresent()) {
             User retrievedUser = optionalUser.get();
+            logger.debug("User found for update: {}", retrievedUser);
 
             retrievedUser.setLogin(userDTO.login());
             retrievedUser.setPassword(userDTO.password());
             retrievedUser.setRole(userDTO.role());
 
-            return new UserDTO(repository.save(retrievedUser));
+            User updatedUser = repository.save(retrievedUser);
+            logger.info("User with ID: {} updated successfully.", updatedUser.getId());
+            return new UserDTO(updatedUser);
         } else {
+            logger.error("User with ID: {} not found for update.", userDTO.id());
             throw new UserNotFoundException("User with ID: " + userDTO.id() + " not found.");
         }
     }
 
     @Override
     public UserDTO findById(UUID id) {
+        logger.info("Searching for user with ID: {}", id);
         Optional<User> optionalUser = repository.findById(id);
+
         if (optionalUser.isPresent()) {
+            logger.info("User with ID: {} found successfully.", id);
             return new UserDTO(optionalUser.get());
         } else {
+            logger.error("User with ID: {} not found.", id);
             throw new UserNotFoundException("User with ID: " + id + " not found.");
         }
     }
 
     @Override
     public List<UserDTO> listAll() {
+        logger.info("Listing all registered users.");
         List<User> userList = repository.findAll();
+        logger.info("Total users found: {}", userList.size());
         return userList.stream()
                 .map(UserDTO::new)
                 .collect(Collectors.toList());
@@ -61,10 +79,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void delete(UUID id) {
+        logger.info("Attempting to delete user with ID: {}", id);
         Optional<User> optionalUser = repository.findById(id);
+
         if (optionalUser.isPresent()) {
             repository.deleteById(id);
+            logger.info("User with ID: {} deleted successfully.", id);
         } else {
+            logger.error("User with ID: {} not found for deletion.", id);
             throw new UserNotFoundException("User with ID: " + id + " not found.");
         }
     }
