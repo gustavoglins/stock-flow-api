013abcde27e3e515ac6ac8e3a1959e405855fccb
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-19 15:41:07 -0300

    feat: create Product entity and DTO

---
+++ src/main/java/com/stockflow/dto/ProductDTO.java
@@ -0,0 +1,34 @@
+package com.stockflow.dto;
+
+import com.stockflow.model.product.Product;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public record ProductDTO(
+
+        Long id,
+
+        @NotBlank(message = "Product name cannot be blank or empty")
+        String name,
+
+        @NotBlank(message = "Product description cannot be blank or empty")
+        String description,
+
+        @Min(message = "Product price must be greater than or equal to 0", value = 0)
+        double price,
+
+        @NotNull(message = "The product quantity must be greater than or equal to 0")
+        @Min(message = "The product quantity must be greater than or equal to 0", value = 0)
+        Long quantity) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ProductDTO(Product product){
+        this(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getQuantity());
+    }
+}
---
+++ src/main/java/com/stockflow/model/product/Product.java
@@ -0,0 +1,93 @@
+package com.stockflow.model.product;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, scale = 11, precision = 2)
+    private double price;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(price, product.price) == 0 && Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(description, product.description) && Objects.equals(quantity, product.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, price, quantity);
+    }
+}
