f3b94635bfd6eeafd2904b9fb526062682797bde
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-22 11:38:13 -0300

    chore: prepare User class for Spring Security implementation

--- src/main/java/com/stockflow/model/user/User.java
+++ src/main/java/com/stockflow/model/user/User.java
@@ -1,16 +1,22 @@
 package com.stockflow.model.user;
 
+import com.stockflow.dto.RegisterDTO;
 import com.stockflow.dto.UserDTO;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -30,6 +36,12 @@ public class User implements Serializable {
     private UserRole role;
 
     public User() {
+    }
+
+    public User(RegisterDTO data){
+        this.login = data.login();
+        this.password = data.password();
+        this.role = data.role();
     }
 
     public User(UserDTO userDTO) {
@@ -64,6 +76,37 @@ public class User implements Serializable {
 
     public void setRole(UserRole role) {
         this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_COMMON"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_COMMON"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
