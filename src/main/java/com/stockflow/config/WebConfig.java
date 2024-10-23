a76e7d908849f657d336bd973754bbe0883a2381
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-23 10:31:10 -0300

    refactor: upgrade in CORS service in WebConfig

--- src/main/java/com/stockflow/config/WebConfig.java
+++ src/main/java/com/stockflow/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.stockflow.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "http://localhost:3000,http://localhost:8080,https://gustavoglins.com";
+
     @Override
     public void addCorsMappings(CorsRegistry registry){
+        var allowedOrigins = corsOriginPatterns.split(",");
         registry.addMapping("/**") // Allow CORS in all endpoints
-                .allowedOrigins("gustavoglins.com") // Allowed domains
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed methods http
+                .allowedOrigins(allowedOrigins) // Allowed domains
+                .allowedMethods("*") // Allowed methods http
                 .allowedHeaders("*") // Allowed headers (Authorization, Content-Type, ...)
-                .allowCredentials(true); // Allow cookies
+                .allowCredentials(true); // Allow cookies and authentication
     }
 }
