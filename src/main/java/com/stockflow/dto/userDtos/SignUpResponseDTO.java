c291f17da89fce722764dac362327219593e66b6
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-24 00:29:24 -0300

    refactor: adding JsonProperty annotations in SignUpResponseDTO

--- src/main/java/com/stockflow/dto/userDtos/SignUpResponseDTO.java
+++ src/main/java/com/stockflow/dto/userDtos/SignUpResponseDTO.java
@@ -1,11 +1,28 @@
 package com.stockflow.dto.userDtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.stockflow.model.user.User;
 import com.stockflow.model.user.UserRole;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.UUID;
 
-public record SignUpResponseDTO(UUID id, String login, UserRole role) {
+@JsonPropertyOrder({"id", "login", "role"})
+public record SignUpResponseDTO(
+
+        @JsonProperty("id")
+        UUID id,
+
+        @JsonProperty("login")
+        String login,
+
+        @JsonProperty("role")
+        UserRole role) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public SignUpResponseDTO(User user){
         this(user.getId(), user.getLogin(), user.getRole());
