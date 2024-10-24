55a3445e2a12e159c74849c89dfafd65531b9342
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-24 00:25:25 -0300

    refactor: adding Validations in SignInRequestDTO

--- src/main/java/com/stockflow/dto/userDtos/SignInRequestDTO.java
+++ src/main/java/com/stockflow/dto/userDtos/SignInRequestDTO.java
@@ -1,4 +1,18 @@
 package com.stockflow.dto.userDtos;
 
-public record SignInRequestDTO(String login, String password) {
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public record SignInRequestDTO(
+
+        @NotBlank(message = "Login cannot be blank or empty")
+        String login,
+
+        @NotBlank(message = "Password cannot be blank or empty")
+        String password) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 }
