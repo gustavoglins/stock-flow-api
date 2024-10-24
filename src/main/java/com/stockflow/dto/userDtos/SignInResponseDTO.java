b82c2f33a38b8f24b22f1e9c704207181970ce60
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-24 00:26:42 -0300

    refactor: adding JsonProperty annotations in SigInResponseDTO

--- src/main/java/com/stockflow/dto/userDtos/SignInResponseDTO.java
+++ src/main/java/com/stockflow/dto/userDtos/SignInResponseDTO.java
@@ -1,4 +1,17 @@
 package com.stockflow.dto.userDtos;
 
-public record SignInResponseDTO(String token) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@JsonPropertyOrder({"token"})
+public record SignInResponseDTO(
+
+        @JsonProperty("token")
+        String token) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 }
