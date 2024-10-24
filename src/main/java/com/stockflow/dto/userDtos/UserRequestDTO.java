5dc805e60717e9358cbb1585da785ff58a5fe019
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-23 23:00:04 -0300

    refactor: cleaning code

--- src/main/java/com/stockflow/dto/userDtos/UserRequestDTO.java
+++ src/main/java/com/stockflow/dto/userDtos/UserRequestDTO.java
@@ -1,8 +1,5 @@
 package com.stockflow.dto.userDtos;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.stockflow.model.user.User;
 import com.stockflow.model.user.UserRole;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,31 +9,21 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
-@JsonPropertyOrder({"id", "login", "password", "role", "links"})
 public record UserRequestDTO(
 
-        @JsonProperty("id")
         UUID id,
 
-        @JsonProperty("login")
         @NotBlank(message = "User login cannot be blank or empty")
         String login,
 
-        @JsonProperty("password")
         @NotBlank(message = "User password cannot be blank or empty")
         String password,
 
-        @JsonProperty("role")
         @NotNull(message = "User role cannot be empty")
         UserRole role,
 
-        @JsonProperty("links")
         Links links) implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
-
-    public UserRequestDTO(User user) {
-        this(user.getId(), user.getLogin(), user.getPassword(), user.getRole(), user.getLinks());
-    }
 }
